11fbb8cbb7d0080e42082763bc7b84981eeff9db
fsegatz <fabian.segatz@gmail.com>
2022-11-02 23:54:32 +0100

    init

---
+++ release/main/CMakeFiles/index.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/Fabian/developement/personalpage/release/main/index_autogen/mocs_compilation.cpp" "main/CMakeFiles/index.dir/index_autogen/mocs_compilation.cpp.o" "gcc" "main/CMakeFiles/index.dir/index_autogen/mocs_compilation.cpp.o.d"
+  "C:/Users/Fabian/developement/personalpage/main/main.cpp" "main/CMakeFiles/index.dir/main.cpp.o" "gcc" "main/CMakeFiles/index.dir/main.cpp.o.d"
+  "C:/Users/Fabian/developement/personalpage/main/mainwindow.cpp" "main/CMakeFiles/index.dir/mainwindow.cpp.o" "gcc" "main/CMakeFiles/index.dir/mainwindow.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ release/main/CMakeFiles/index.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/index_autogen.dir/AutogenUsed.txt"
+  "CMakeFiles/index_autogen.dir/ParseCache.txt"
+  "index_autogen"
+  "CMakeFiles/index.dir/index_autogen/mocs_compilation.cpp.o"
+  "CMakeFiles/index.dir/index_autogen/mocs_compilation.cpp.o.d"
+  "CMakeFiles/index.dir/main.cpp.o"
+  "CMakeFiles/index.dir/main.cpp.o.d"
+  "CMakeFiles/index.dir/mainwindow.cpp.o"
+  "CMakeFiles/index.dir/mainwindow.cpp.o.d"
+  "index.js"
+  "index.js.manifest"
+  "index.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/index.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
